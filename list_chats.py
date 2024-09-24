5acbdace07a768b4c42aed0abe3e7d8ab9025d67
Vladimir <bortzvk@gmail.com>
2024-09-24 23:26:25 +0200

    fixes

--- list_chats.py
+++ list_chats.py
@@ -17,13 +17,8 @@ async def list_chats():
         chat_id = dialog.id
         chat_name = dialog.name
 
-        # Исключаем личные чаты и каналы
-        if dialog.is_group and not dialog.is_channel:
-            # Если чат приватный, то ID начинается с -100
-            if chat_id < 0:
-                chat_id = f"-100{abs(chat_id)}"
-
-            print(f'Название: {chat_name}, ID: {chat_id}')
+        # Логируем тип чата
+        print(f'Название: {chat_name}, ID: {chat_id}, Личный: {dialog.is_user}, Группа: {dialog.is_group}, Канал: {dialog.is_channel}')
 
 # Запускаем клиент и выполняем функцию получения списка чатов
 with client:
