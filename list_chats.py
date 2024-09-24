ec78580bc9491c5e8a404bd4fcee03772696951b
Vladimir <bortzvk@gmail.com>
2024-09-24 23:17:47 +0200

    added correct list chats script

--- list_chats.py
+++ list_chats.py
@@ -17,11 +17,13 @@ async def list_chats():
         chat_id = dialog.id
         chat_name = dialog.name
 
-        # Если чат приватный, то ID начинается с -100
-        if chat_id < 0:
-            chat_id = f"-100{abs(chat_id)}"
+        # Исключаем личные чаты и каналы
+        if dialog.is_group and not dialog.is_channel:
+            # Если чат приватный, то ID начинается с -100
+            if chat_id < 0:
+                chat_id = f"-100{abs(chat_id)}"
 
-        print(f'Название: {chat_name}, ID: {chat_id}')
+            print(f'Название: {chat_name}, ID: {chat_id}')
 
 # Запускаем клиент и выполняем функцию получения списка чатов
 with client:
